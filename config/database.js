1f2826bdab9723170fa110d0daba30a847a52f21
Asiah Bennett Dev <60096195+asiahbennettdev@users.noreply.github.com>
2020-03-23 10:00:35 -0400

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://DemoDay:draftone@demo-day-draft-one-0fps0.mongodb.net/test?retryWrites=true', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'Demo-Day-Draft-One'
+};
